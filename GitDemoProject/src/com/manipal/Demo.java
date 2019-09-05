821a2332ef857899d08fc28eee964e542db5229f
ITFS <ITFS@172.24.84.37>
2019-09-05 11:28:49 +0530

    copying the new second file

--- GitDemoProject/src/com/manipal/Demo.java
+++ GitDemoProject/src/com/manipal/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("neha");
+		System.out.println("neha..!!");
+		System.out.println("second line added");
 	}
 
 }
